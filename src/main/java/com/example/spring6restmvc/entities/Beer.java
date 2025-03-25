5cf28c2b1eef706df1a39905ce81eee26acddafb
Кристиян Николов <stu2201261057@uni-plovdiv.bg>
2025-03-25 20:49:47 +0200

    Added Mysql to the project and the ability to migrate database through Flyway

--- src/main/java/com/example/spring6restmvc/entities/Beer.java
+++ src/main/java/com/example/spring6restmvc/entities/Beer.java
@@ -5,7 +5,9 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -21,7 +23,8 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @UuidGenerator
-    @Column(updatable = false, nullable = false, columnDefinition = "varchar")
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36,updatable = false, nullable = false, columnDefinition = "varchar(36)")
     private UUID id;
     @Version
     private Integer version;
